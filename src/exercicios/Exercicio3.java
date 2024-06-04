69b03715f43298dfdc76ac292f29c2e76ffe39fa
Beazinat <160698088+beazinat@users.noreply.github.com>
2024-06-04 19:07:37 -0300

    Update Exercicio3.java

--- src/exercicios/Exercicio3.java
+++ src/exercicios/Exercicio3.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 public class Exercicio3 {
 
 	public static void main(String[] args) {
-		Scanner leia = new Scanner(System.in);
+	Scanner leia = new Scanner(System.in);
 		
-		float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
+	float salarioBruto, adicionalNoturno, horasExtras, descontos, salarioLiquido;
 		
-		System.out.printf("Salário Bruto: ");
-		salarioBruto = leia.nextFloat();
+	System.out.printf("Salário Bruto: ");
+	salarioBruto = leia.nextFloat();
 		
-		System.out.printf("Adicional Noturno: ");
-		adicionalNoturno = leia.nextFloat();
+	System.out.printf("Adicional Noturno: ");
+	adicionalNoturno = leia.nextFloat();
 		
-		System.out.printf("Horas Extras: ");
-		horasExtras = leia.nextFloat();
+	System.out.printf("Horas Extras: ");
+	horasExtras = leia.nextFloat();
 		
-		System.out.printf("Descontos: ");
-		descontos = leia.nextFloat();
+	System.out.printf("Descontos: ");
+	descontos = leia.nextFloat();
 		
-		leia.close();
+	leia.close();
 		
-		salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
+	salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos;
 
-		System.out.printf("\nSalário Líquido: %.2f", salarioLiquido);
+	System.out.printf("\nSalário Líquido: %.2f", salarioLiquido);
 	}
 
 }
